499e2acd642e742d7000eebbab50555d91b32202
dimas lópez <me@dimaslz.io>
2020-09-29 17:16:17 +0200

    fix(test): Fix tests fix(test): Add tailwindCSS to test environment

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-tailwind app is running!');
+    expect(compiled.querySelector('.App .App__title').textContent).toContain('Angular + TailwindCSS + Sass');
   });
 });
